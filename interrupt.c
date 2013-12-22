7b70bf48e6f5862175bf44f36c758321090b0a02
Marc Cobos <cobos@MacBook-Pro-de-Marc.local>
2013-12-22 09:59:49 +0100

    Fork

--- interrupt.c
+++ interrupt.c
@@ -97,9 +97,10 @@ void clock_routine()
       ++zeos_ticks;
       zeos_show_clock();
      
-      /*if (zeos_ticks == 200) {
-	task_switch(idle_task);
-      }*/
+      update_sched_data_rr();
+      if (needs_sched_rr()) {
+          //Canvi de contexte;
+      }
 }
 
 
